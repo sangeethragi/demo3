ee0f5a908d24c9afcf3ed6fa3c036e5bc4a7c162
sangeethragi <sangeeth.ragi@gmail.com>
2020-12-18 16:33:27 +0530

    Update calc.java

--- demo2/src/demo2/calc.java
+++ demo2/src/demo2/calc.java
@@ -3,15 +3,15 @@ package demo2;
 public class calc {
 	
 	public void add(int a,int b) {
-		int n=a+b;
+		int m=(a+b);
 		System.out.println(n);
 	}
 	public void subs(int a,int b) {
-		int n=a-b;
+		int m=(a-b);
 		System.out.println(n);
 	}
 	public void prod(int a,int b) {
-		int n=a*b;
+		int m=(a*b);
 		System.out.println(n);
 
 }
